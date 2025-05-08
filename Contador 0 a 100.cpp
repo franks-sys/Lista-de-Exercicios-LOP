eb7765704ec6455266e7b36e1391bd586a87c0ae
frankss <franciscopaulasouza3@gmail.com>
2025-05-08 13:42:09 -0300

    Contador de 1 a 100

---
+++ Contador 0 a 100.cpp
@@ -0,0 +1,17 @@
+//Biblioteca
+#include <stdio.h>
+
+
+int main (){
+//Inicio
+	
+	int i;
+	//variavel atribuida para ser usada no for
+	
+	for (i = 1; i <= 100; i++){
+		printf("%d\n", i);
+	}
+	//extrutura do contador para começar no numero 1 e acrescentar mais 1 ate chegar no 100
+	
+	return 0;
+}
